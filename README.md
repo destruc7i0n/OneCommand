7da62c54705897b7ac52879e1b819aad0b9ec781
yrsegal <yrsegal@gmail.com>
2015-08-27 12:42:17 -0400

    all kinds of fun things
    also bundles pyperclip
    closes #1

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 # OneCommand
 A simple python script for one command creations in 1.9
 
-### You need pyperclip for this:
-```python
-pip install pyperclip
-```
-
 ### Windows .exe download
 http://www.mediafire.com/download/vq4572h64k61e2t/oneCommand_v0.2.zip
 
--- oneCommand.py
+++ oneCommand.py
@@ -1,50 +1,109 @@
 import pyperclip
+
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--manual", help="Uses manual mode", dest="manual", action="store_true")
+parser.add_argument("-i", "--instant", help="Uses instant mode", dest="instant", action="store_false")
+parser.add_argument("-f", "--command_file", help="File to load commands from", dest="filepath", action="store",
+	default="")
+args = parser.parse_args()
+
+print """
+----------------------------------------
+TheDestruc7i0n's 1.9 "One Command" Creator
+
+(Type `INIT:` before a command to make it execute only once)
+(Type `COND:` before a command to make it conditional)
+(Please report bugs to me on Twitter @TheDestruc7i0n with detailed information)
+(A known bug is when you have only one constant command and multiple "INIT:" commands. 
+Also, you cannot have only one "INIT:" command and no constant commands.)
+----------------------------------------
+"""
+
+class Command:
+	def __init__(self, cmd, conditional):
+		self.cmd = cmd
+		self.cond = conditional
+	def __str__(self):
+		return self.cmd
+
 endBraces = 0
 x = 1
-commands = []
 once = []
-print "----------------------------------------\nTheDestruc7i0n's 1.9 \"One Command\" Creator\n\n(Type \"INIT:\" before a command to make it execute only once)\n(Please report bugs to me on Twitter @TheDestruc7i0n with detailed information)\n(A known bug is when you have only one constant command and multiple \"INIT:\" commands. Also, you cannot have only one \"INIT:\" command and no constant commands.)\n----------------------------------------\n"
-mi = raw_input("Manual (m) or Instant (i)? ")
-if mi == "m":
-	mi = True
-elif mi == "i":
-	mi = False
+if not args.manual and args.instant:
+	mi = raw_input("Manual (m) or Instant (i)? ")
+	if mi == "m":
+		mi = True
+	elif mi == "i":
+		mi = False
+	else:
+		raise ValueError("Not manual or instant")
 else:
-	raise Exception("Error")
+	mi = args.manual if args.manual else args.instant
 
-inp = raw_input("Command "+str(x)+": ")
-while inp != "":
-	x+=1
-	commands.append(str(inp))
+cmdtext = []
+if not args.filepath:
 	inp = raw_input("Command "+str(x)+": ")
+	while inp != "":
+		x+=1
+		cmdtext.append(str(inp))
+		inp = raw_input("Command "+str(x)+": ")
+elif args.filepath == "stdin":
+	cmdtext = sys.stdin.read().split("\n")
+else:
+	import os.path
+	if os.path.exists(args.filepath):
+		cmdtext = open(args.filepath).read().split("\n")
+	else:
+		raise IOError("File "+args.filepath+" not found.")
 
-for testing in commands[:]:
-	if testing[:5] == "INIT:":
-		once.append(testing.replace("INIT:",""))
-		commands.remove(testing)
+commands = []
+for cmd in cmdtext[:]:
+	init = False
+	cond = False
+	if cmd[:5] == "INIT:":
+		cmd = cmd[5:]
+		init = True
+		if cmd[:5] == "COND:":
+			cmd = cmd[5:]
+			cond = True
+	elif cmd[:5] == "COND:":
+		cmd = cmd[5:]
+		cond = True
+		if cmd[:5] == "INIT:":
+			cmd = cmd[5:]
+			init = True
+	if init:
+		once.append(Command(cmd, cond))
+	else:
+		commands.append(Command(cmd, cond))
 
 if mi:
 	if len(commands) > 0:
 		commands.reverse()
 		once.reverse()
+		final = ""
 		for eachCommand in commands:
+			conditional = "conditional:1b," if eachCommand.cond else ""
 			if len(commands) == 1 and len(once) <= 0:
-				final = "/setblock ~ ~1 ~ repeating_command_block 1 replace {Command:"+str(eachCommand)+"}"
+				final = "setblock ~ ~1 ~ repeating_command_block 1 replace {"+ conditional +"Command:"+str(eachCommand)+"}"
 			else:
 				if len(commands) == 1:
 					commands.append("")
 				if eachCommand == commands[0]:
 					if len(commands) != 2:
-						final = "/summon FallingSand ~ ~1 ~ {Block:chain_command_block,TileEntityData:{Command:"+str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
+						final = "summon FallingSand ~ ~1 ~ {Block:chain_command_block,TileEntityData:{"+ conditional +"Command:"+str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
 						endBraces += 2
 					else:
-						final = "/summon FallingSand ~ ~1 ~ {Block:chain_command_block,TileEntityData:{Command:"+str(eachCommand)
+						final = "summon FallingSand ~ ~1 ~ {Block:chain_command_block,TileEntityData:{"+ conditional +"Command:"+str(eachCommand)
 						endBraces += 1
 				elif eachCommand == commands[-2]:
 					final += str(eachCommand)
 				elif eachCommand == commands[-1]:
 					if len(once) > 0:
-						final += "},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"+str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:fill ~ ~ ~ ~ ~-"+str(len(once))+" ~ chain_command_block 1 replace},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
+						final += "},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{"+ conditional +"Command:"+str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:fill ~ ~ ~ ~ ~-"+str(len(once))+" ~ chain_command_block 1 replace},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
 						endBraces += 3
 						for eachOnce in once:
 							if eachOnce == once[-1]:
@@ -54,16 +113,16 @@ if mi:
 								final += str(eachOnce)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
 								endBraces += 1
 					else:
-						final += "},Data:1,Time:1,Riding:{id:FallingSand,Block:repeating_command_block,TileEntityData:{Command:"+str(eachCommand)+"},Data:1,Time:1"
+						final += "},Data:1,Time:1,Riding:{id:FallingSand,Block:repeating_command_block,TileEntityData:{"+ conditional +"Command:"+str(eachCommand)+"},Data:1,Time:1"
 						endBraces += 1
-					final += "".join(["}" for i in xrange(endBraces)])
+					final += "}"*endBraces
 				else:
 					final += str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
 					endBraces += 1
 		if len(final) < 32500:
 			pyperclip.copy(final)
+			sys.stdout.write(final)
 			print "\nCommand (copied to clipboard): \n\n"+final
-			raw_input("\nWindows: Right click top screen bar, click Edit > Mark, select the command, hit enter.\nOSX: idk\n\nHit enter to close...")
 		else:
 			raise Exception("Too Large")
 	else:
@@ -73,27 +132,28 @@ else:
 		commands.reverse()
 		once.reverse()
 		for eachCommand in commands:
+			conditional = "conditional:1b," if eachCommand.cond else ""
 			if len(commands) == 1 and len(once) <= 0:
-				final = "/setblock ~ ~1 ~ repeating_command_block 1 replace {Command:"+str(eachCommand)+",auto:1b}"
+				final = "setblock ~ ~1 ~ repeating_command_block 1 replace {"+ conditional +"Command:"+str(eachCommand)+",auto:1b}"
 			else:
 				if len(commands) == 1:
 					commands.append("")				
 				if eachCommand == commands[0]:
 					extraADD = 2 if len(once) >= 1 else 0
 					if len(commands) != 2:
-						final = "/summon FallingSand ~ ~1 ~ {Block:redstone_block,Time:1,Riding:{id:FallingSand,Block:command_block,TileEntityData:{Command:setblock ~ ~-"+str((len(commands)+len(once))+1+extraADD)+" ~ redstone_block},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"+str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
+						final = "summon FallingSand ~ ~1 ~ {Block:redstone_block,Time:1,Riding:{id:FallingSand,Block:command_block,TileEntityData:{Command:setblock ~ ~-"+str((len(commands)+len(once))+1+extraADD)+" ~ redstone_block},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{"+ conditional +"Command:"+str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
 						endBraces += 4
 					elif len(commands) == 1:
-						final = "/summon FallingSand ~ ~1 ~ {Block:redstone_block,Time:1,Riding:{id:FallingSand,Block:command_block,TileEntityData:{Command:setblock ~ ~-"+str((len(commands)+len(once))+1+extraADD)+" ~ redstone_block},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"+str(eachCommand)
+						final = "summon FallingSand ~ ~1 ~ {Block:redstone_block,Time:1,Riding:{id:FallingSand,Block:command_block,TileEntityData:{Command:setblock ~ ~-"+str((len(commands)+len(once))+1+extraADD)+" ~ redstone_block},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{"+ conditional +"Command:"+str(eachCommand)
 						endBraces += 3
 					else:
-						final = "/summon FallingSand ~ ~1 ~ {Block:redstone_block,Time:1,Riding:{id:FallingSand,Block:command_block,TileEntityData:{Command:setblock ~ ~-"+str((len(commands)+len(once))+1+extraADD)+" ~ redstone_block},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"+str(eachCommand)
+						final = "summon FallingSand ~ ~1 ~ {Block:redstone_block,Time:1,Riding:{id:FallingSand,Block:command_block,TileEntityData:{Command:setblock ~ ~-"+str((len(commands)+len(once))+1+extraADD)+" ~ redstone_block},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{"+ conditional +"Command:"+str(eachCommand)
 						endBraces += 3
 				elif eachCommand == commands[-2]:
 					final += str(eachCommand)
 				elif eachCommand == commands[-1]:
 					if len(once) > 0:
-						final += "},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"+str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:fill ~ ~ ~ ~ ~-"+str(len(once))+" ~ chain_command_block 1 replace},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
+						final += "},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{"+ conditional +"Command:"+str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:fill ~ ~ ~ ~ ~-"+str(len(once))+" ~ chain_command_block 1 replace},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
 						endBraces += 3
 						for eachOnce in once:
 							if eachOnce == once[-1]:
@@ -103,17 +163,17 @@ else:
 								final += str(eachOnce)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
 								endBraces += 1
 					else:	
-						final += "},Data:1,Time:1,Riding:{id:FallingSand,Block:repeating_command_block,TileEntityData:{Command:"+str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:stone,Time:1}"
+						final += "},Data:1,Time:1,Riding:{id:FallingSand,Block:repeating_command_block,TileEntityData:{"+ conditional +"Command:"+str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:stone,Time:1}"
 						endBraces += 1
-					final += "".join(["}" for i in xrange(endBraces)])
+					final += "}"*endBraces
 				else:
 					final += str(eachCommand)+"},Data:1,Time:1,Riding:{id:FallingSand,Block:chain_command_block,TileEntityData:{Command:"
 					endBraces += 1
 
 		if len(final) < 32500:
 			pyperclip.copy(final)
+			sys.stdout.write(final)
 			print "\nCommand (copied to clipboard): \n\n"+final
-			raw_input("\nWindows: Right click top screen bar, click Edit > Mark, select the command, hit enter.\nOSX: idk\n\nHit enter to close...")
 		else:
 			raise Exception("Too Large")
 	else:
